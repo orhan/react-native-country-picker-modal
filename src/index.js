ff9e91e914b8548dede6bc50b0afa04b21716fea
Orhan Sönmez <orhnsnmz@gmail.com>
2016-08-07 23:24:53 +0200

    Added possibility to render a custom view as header if desired

--- src/index.js
+++ src/index.js
@@ -173,6 +173,7 @@ class CountryPicker extends Component {
             scrollsToTop={true}>
             {_.map(this.state.countries, (country, index) => this._renderCountry(country, index))}
           </ScrollView>*/}
+          {this.props.renderHeader()}
           <ListView
             style={this.props.modalStyle}
             contentContainerStyle={[styles.contentContainer, this.props.modalListStyle]}
